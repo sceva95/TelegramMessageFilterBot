43e54c60c7db4888523dae6533de1db3b19cfcd1
Valentino Scevaroli <valentino.scevaroli@mia-platform.eu>
2024-10-09 22:43:55 +0200

    MessageFilter - v1.0.0

---
+++ changelog.md
@@ -0,0 +1,1 @@
+- [v1.0.0] : MessageFilter working with a file of keywords, first run need to be approved from telegram app
--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, events
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from dotenv import load_dotenv
 import os
+import re
+from datetime import datetime
 
 load_dotenv()
 
@@ -28,39 +28,34 @@ def load_keywords():
         print(f"Errore durante il caricamento delle parole chiave: {e}")
         return []
 
-# Funzione per filtrare i messaggi
-async def filter_notifications(message):
-    keywords = load_keywords() # Parole chiave da filtrare
-    message_text = message.text.lower()
-    channel_name = message.chat.title if hasattr(message.chat, 'title') else "Nome canale non disponibile"
-    # Se il messaggio contiene una delle parole chiave
-    matched_keywords = [keyword for keyword in keywords if keyword.lower() in message_text]
-    
-    # Se ci sono parole chiave corrispondenti
-    if matched_keywords:
-        matched_keywords_str = ', '.join(matched_keywords)  # Unisci le parole chiave abbinate in una stringa
-        print(f"Notifica importante trovata nel canale '{message.peer_id}': {message.text}, matcha {matched_keywords_str}")
-        # Invia il messaggio filtrato al bot includendo le parole chiave abbinate
-        await send_to_bot(message.text, channel_name, matched_keywords_str)
+keywords = load_keywords()
 
-async def send_to_bot(filtered_message, channel_name, matched_keywords):
+keywords_pattern = '|'.join(map(re.escape, keywords))  # Unisci le parole con OR (|)
+
+async def send_to_bot(filtered_message, channel_name):
     # Messaggio da inviare al bot, includendo il nome del canale e le parole chiave abbinate
     message_to_send = (
         f"Messaggio dal canale '{channel_name}': {filtered_message}\n"
-        f"Parole chiave abbinate: {matched_keywords}"
     )
     chat_id = os.getenv("CHAT_ID")  # Sostituisci con l'ID della chat in cui vuoi inviare il messaggio
-    await client.send_message(chat_id, message_to_send)  # Usa il client Telethon per inviare il messaggio
+    try:
+        await client.send_message(chat_id, message_to_send)  # Usa il client Telethon per inviare il messaggio
+        print(f"Messaggio inviato correttamente!")
+    except Exception as e:
+        print(f"Eccezione rilevata: ${e}")
+
 
 # Evento per gestire i messaggi da canali
-@client.on(events.NewMessage(chats=channel_usernames))
+@client.on(events.NewMessage(pattern=f"(?i).*({keywords_pattern}).*"))
 async def handler(event):
     message = event.message
-    await filter_notifications(message)
+    channel_name = message.chat.title if hasattr(message.chat, 'title') else "Nome canale non disponibile"
+    print(f"Messaggio ricevuto: Canale ${channel_name}; \nTimestamp: ${datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+
+    await send_to_bot(message.text, channel_name)
 
 # Funzione principale per eseguire il bot
 async def start_bot():
-
     # Avvia il client di Telethon
     await client.start()
     print("Bot in esecuzione...")
